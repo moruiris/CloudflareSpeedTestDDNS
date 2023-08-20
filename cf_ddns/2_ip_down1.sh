d9363007185184b79c325e91e60a466d82f0921a
moruiris <53965900+moruiris@users.noreply.github.com>
2023-08-20 17:26:38 +0800

    Create 2_ip_down1.sh

---
+++ cf_ddns/2_ip_down1.sh
@@ -0,0 +1,7 @@
+if [ -e ./cf_ddns/tmp/ ]; then
+	rm -rf ./cf_ddns/tmp/
+URL="https://github.com/XIU2/CloudflareSpeedTest/releases/download/$VERSION/CloudflareST_linux_amd64.tar.gz"
+wget -P ./cf_ddns/tmp/ ${PROXY}$URL
+tar -zxf ./cf_ddns/tmp/CloudflareST_linux_*.tar.gz -C ./cf_ddns/tmp/
+mv ./cf_ddns/tmp/ip.txt ./cf_ddns/tmp/ipv6.txt ./cf_ddns/
+rm -rf ./cf_ddns/tmp/
