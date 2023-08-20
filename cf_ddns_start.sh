24a5cebf23d8f1d91eb20e09af4f4f51baf4ab7f
moruiris <53965900+moruiris@users.noreply.github.com>
2023-08-20 19:58:24 +0800

    Update cf_ddns_start.sh

--- cf_ddns_start.sh
+++ cf_ddns_start.sh
@@ -3,4 +3,20 @@ source config.conf;
 
 source ./cf_ddns/1_check.sh
 
+case $IP_SELECT in
+    1)
+        source ./cf_ddns/2_ip_down1.sh
+        ;;
+    2)
+        source ./cf_ddns/2_ip_down2.sh
+        ;;
+    *)
+        echo "未选择ip文件，不进行最新ip文件下载"
+        ;;
+esac
+
+source ./cf_ddns/3_cf_ddns_cloudflare.sh
+
+source ./cf_ddns/4_cf_push.sh
+
 exit 0;
