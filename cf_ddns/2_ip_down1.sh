505ad801238b3b7250fd20eb95beb9dddb88ea33
moruiris <53965900+moruiris@users.noreply.github.com>
2023-08-20 18:57:22 +0800

    Update 2_ip_down1.sh

--- cf_ddns/2_ip_down1.sh
+++ cf_ddns/2_ip_down1.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#         下载ip文件。
+
 if [ -e ./cf_ddns/tmp/ ]; then
 	rm -rf ./cf_ddns/tmp/
 URL="https://github.com/XIU2/CloudflareSpeedTest/releases/download/$VERSION/CloudflareST_linux_amd64.tar.gz"
@@ -5,3 +8,4 @@ wget -P ./cf_ddns/tmp/ ${PROXY}$URL
 tar -zxf ./cf_ddns/tmp/CloudflareST_linux_*.tar.gz -C ./cf_ddns/tmp/
 mv ./cf_ddns/tmp/ip.txt ./cf_ddns/tmp/ipv6.txt ./cf_ddns/
 rm -rf ./cf_ddns/tmp/
+fi
