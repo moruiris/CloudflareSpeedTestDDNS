662b801021f6c8a5a2846f32e9697ff88bba8387
moruiris <53965900+moruiris@users.noreply.github.com>
2023-08-26 20:14:26 +0800

    Update 4_push.sh

--- cf_ddns/4_push.sh
+++ cf_ddns/4_push.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 ##用于CloudflareSpeedTestDDNS执行情况推送。
 
+echo "TG将推送的内容如下：";
+
+cat $informlog
+
 message_text=$(echo "$(sed "$ ! s/$/\\\n/ " ./cf_ddns/informlog | tr -d '\n')")
 
 ##tg推送##
 if [[ -z ${Proxy_TG} ]]; then
-   tgapi="https://api.telegram.org"
+	tgapi="https://api.telegram.org"
 else
-   tgapi=$Proxy_TG
+	tgapi=$Proxy_TG
 fi
+
 TGURL="$tgapi/bot${telegramBotToken}/sendMessage"
+
 if [[ -z ${telegramBotToken} ]]; then
-   echo "未配置TG推送"
+	echo "未配置TG推送"
 else
-   res=$(timeout 20s curl -s -X POST $TGURL -H "Content-type:application/json" -d '{"chat_id":"'$telegramBotUserId'", "parse_mode":"HTML", "text":"'$message_text'"}')
-
-   if [ $? == 124 ];then
-      echo 'TG_api请求超时,请检查网络是否重启完成并是否能够访问TG'
-   fi
-
-   resSuccess=$(echo "$res" | jq -r ".ok")
-   if [[ $resSuccess = "true" ]]; then
-      echo "TG推送成功";
-   else
-      echo "TG推送失败，请检查网络或TG机器人token和ID";
-   fi
+	res=$(timeout 20s curl -s -X POST $TGURL -H "Content-type:application/json" -d '{"chat_id":"'$telegramBotUserId'", "parse_mode":"HTML", "text":"'$message_text'"}')
+	if [ $? == 124 ];then
+		echo 'TG_api请求超时,请检查网络是否重启完成并是否能够访问TG'
+	fi
+	resSuccess=$(echo "$res" | jq -r ".ok")
+	if [[ $resSuccess = "true" ]]; then
+		echo "TG推送成功";
+	else
+		echo "TG推送失败，请检查网络或TG机器人token和ID";
+	fi
 fi
-####
-
-cat $informlog
 
 exit 0;
