dcd60f84ca393ace09e6a620478dd3d69d4d3078
moruiris <53965900+moruiris@users.noreply.github.com>
2023-08-26 20:16:10 +0800

    Update 1_check.sh

--- cf_ddns/1_check.sh
+++ cf_ddns/1_check.sh
@@ -10,22 +10,22 @@ cf_push="./cf_ddns/4_push.sh"
 
 # 初始化推送
 if [ -e ${informlog} ]; then
-  rm ${informlog}
+	rm ${informlog}
 fi
 # 检测是否配置DDNS
 if [[ -z ${api_key} ]]; then
-  IP_TO_CF=0
+	IP_TO_CF=0
 else
-  IP_TO_CF=1
+	IP_TO_CF=1
 fi
 
 if [ $IP_TO_CF -eq 1 ];then
-  echo "配置获取成功！"
+	echo "配置获取成功！"
 else
-  echo "cf_ddns未配置！！！"
-  echo "cf_ddns未配置！！！" > $informlog
-  source $cf_push;
-  exit 1;
+	echo "cf_ddns未配置！！！"
+	echo "cf_ddns未配置！！！" > $informlog
+	source $cf_push;
+	exit 1;
 fi
 
 # 如果是开机第一次运行的话，将进行初始化；或者强制运行初始化
@@ -36,96 +36,96 @@ if [ ! -e "$flag_file" ] || [ "$flag" = "1" ]; then
 
 	# 检查bash是否安装
 	if ! command -v bash &> /dev/null; then
-	    echo "bash not found. Adding to the list of required packages."
-	    packages="$packages bash"
+		echo "bash not found. Adding to the list of required packages."
+		packages="$packages bash"
 	else
-	    echo "bash is already installed."
+		echo "bash is already installed."
 	fi
 
 	# 检查jq是否安装
 	if ! command -v jq &> /dev/null; then
-	    echo "jq not found. Adding to the list of required packages."
-	    packages="$packages jq"
+		echo "jq not found. Adding to the list of required packages."
+		packages="$packages jq"
 	else
-	    echo "jq is already installed."
+		echo "jq is already installed."
 	fi
 	
 	# 检查wget是否安装
 	if ! command -v wget &> /dev/null; then
-	    echo "wget not found. Adding to the list of required packages."
-	    packages="$packages wget"
+		echo "wget not found. Adding to the list of required packages."
+		packages="$packages wget"
 	else
-	    echo "wget is already installed."
+		echo "wget is already installed."
 	fi
 	
 	# 检查curl是否安装
 	if ! command -v curl &> /dev/null; then
-	    echo "curl not found. Adding to the list of required packages."
-	    packages="$packages curl"
+		echo "curl not found. Adding to the list of required packages."
+		packages="$packages curl"
 	else
-	    echo "curl is already installed."
+		echo "curl is already installed."
 	fi
 	
 	# 检查tar是否安装
 	if ! command -v tar &> /dev/null; then
-	    echo "tar not found. Adding to the list of required packages."
-	    packages="$packages tar"
+		echo "tar not found. Adding to the list of required packages."
+		packages="$packages tar"
 	else
 	    #由于有的设备有tar但版本过低还是无法正常解压，所以tar强制更新
-	    packages="$packages tar"
-	    echo "tar is already installed."
+		packages="$packages tar"
+		echo "tar is already installed."
 	fi
- 
+	
 	# 检查sed是否安装
 	if ! command -v sed &> /dev/null; then
-	    echo "sed not found. Adding to the list of required packages."
-	    packages="$packages sed"
+		echo "sed not found. Adding to the list of required packages."
+		packages="$packages sed"
 	else
-	    echo "sed is already installed."
+	   	echo "sed is already installed."
 	fi
 	
 	# 检查awk是否安装
 	if ! command -v awk &> /dev/null; then
-	    echo "awk not found. Adding to the list of required packages."
-	    packages="$packages gawk"
+	    	echo "awk not found. Adding to the list of required packages."
+	    	packages="$packages gawk"
 	else
-	    echo "awk is already installed."
+	    	echo "awk is already installed."
 	fi
 	
 	# 检查tr是否安装
 	if ! command -v tr &> /dev/null; then
-	    echo "tr not found. Adding to the list of required packages."
-	    packages="$packages coreutils"
+	    	echo "tr not found. Adding to the list of required packages."
+	    	packages="$packages coreutils"
 	else
-	    echo "tr is already installed."
+	    	echo "tr is already installed."
 	fi
 
 	# 判断系统进行安装
 	if [ -n "$packages" ]; then
-	    echo "The following packages are required: $packages"
-	    if grep -qi "alpine" /etc/os-release; then
-	        echo "Installing packages using apk..."
-	        apk update
-	        apk add $packages
-	    elif grep -qi "openwrt" /etc/os-release; then
-	        echo "Installing packages using opkg..."
-	        opkg update
-	        opkg install $packages
-	        #openwrt没有安装timeout
-	        opkg install coreutils-timeout
-	    elif grep -qi "ubuntu\|debian" /etc/os-release; then
-	        echo "Installing packages using apt-get..."
-	        sudo apt-get update
-	        sudo apt-get install $packages -y
-	    elif grep -qi "centos\|red hat\|fedora" /etc/os-release; then
-	        echo "Installing packages using yum..."
-	        sudo yum install $packages -y
-	    else
-	        echo "未能检测出你的系统：$(uname)，请自行安装$packages这些软件。"
-	        echo "未能检测出你的系统：$(uname)，请自行安装$packages这些软件。" > $informlog
-	        source $cf_push;
-	        exit 1
-	    fi
+	    	echo "The following packages are required: $packages"
+	    	if grep -qi "alpine" /etc/os-release; then
+	        	echo "Installing packages using apk..."
+	        	apk update
+	        	apk add $packages
+	    	elif grep -qi "openwrt" /etc/os-release; then
+	        	echo "Installing packages using opkg..."
+	        	opkg update
+	        	opkg install $packages
+	        	#openwrt没有安装timeout
+	        	opkg install coreutils-timeout
+	    	elif grep -qi "ubuntu\|debian" /etc/os-release; then
+	        	echo "Installing packages using apt-get..."
+	        	sudo apt-get update
+	        	sudo apt-get install $packages -y
+	    	elif grep -qi "centos\|red hat\|fedora" /etc/os-release; then
+	        	echo "Installing packages using yum..."
+	        	sudo yum install $packages -y
+	    	else
+	        	echo "未能检测出你的系统：$(uname)，请自行安装$packages这些软件。"
+	        	echo "未能检测出你的系统：$(uname)，请自行安装$packages这些软件。" > $informlog
+	        	source $cf_push;
+	        	exit 1
+	    	fi
 	fi
 	# 检测CloudflareST是否安装
 	LATEST_URL=https://api.github.com/repos/XIU2/CloudflareSpeedTest/releases/latest
