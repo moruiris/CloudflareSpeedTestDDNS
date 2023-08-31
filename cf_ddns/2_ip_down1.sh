76be0b29ba80b2e06074617827aa27b03f2961b0
moruiris <53965900+moruiris@users.noreply.github.com>
2023-08-31 22:53:29 +0800

    Update 2_ip_down1.sh

--- cf_ddns/2_ip_down1.sh
+++ cf_ddns/2_ip_down1.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-#         下载ip文件。
+#         下载cf官方ip文件。
 
-if [ -e ./cf_ddns/tmp/ ]; then
-	rm -rf ./cf_ddns/tmp/
-fi
-if [ "$IP_SELECT" = "1" ]; then
+if [ "$IP_UPDATE" = "1" ] || [ ! -f ./cf_ddns/ip1/ip.txt ] || [ ! -f ./cf_ddns/ip1/ipv6.txt ]; then
 	echo "下载cf官方ip文件"
+	if [ -e ./cf_ddns/tmp/ ]; then
+		rm -rf ./cf_ddns/tmp/
+	fi
+	if [ ! -e ./cf_ddns/ip1/ ]; then
+		mkdir ./cf_ddns/ip1/
+	fi
 	LATEST_URL=https://api.github.com/repos/XIU2/CloudflareSpeedTest/releases/latest
 	latest_version() {
   		curl --silent $LATEST_URL | grep "tag_name" | cut -d '"' -f 4
-}
+  		}
 	VERSION=$(latest_version)
 	URL="https://github.com/XIU2/CloudflareSpeedTest/releases/download/$VERSION/CloudflareST_linux_amd64.tar.gz"
 	wget -P ./cf_ddns/tmp/ ${PROXY}$URL
 	tar -zxf ./cf_ddns/tmp/CloudflareST_linux_*.tar.gz -C ./cf_ddns/tmp/
-	mv ./cf_ddns/tmp/ip.txt ./cf_ddns/tmp/ipv6.txt ./cf_ddns/
+	mv -f ./cf_ddns/tmp/ip.txt ./cf_ddns/tmp/ipv6.txt ./cf_ddns/ip1/
 	rm -rf ./cf_ddns/tmp/
+	if [ "$IP_SELECT" = "1" ]; then
+		cp -f ./cf_ddns/ip1/ip.txt ./cf_ddns/ip1/ipv6.txt ./cf_ddns/
+	fi
 fi
