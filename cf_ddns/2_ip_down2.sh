0fa9a8a067867fd607f21fec708b497125a69022
moruiris <53965900+moruiris@users.noreply.github.com>
2023-08-31 22:53:00 +0800

    Update 2_ip_down2.sh

--- cf_ddns/2_ip_down2.sh
+++ cf_ddns/2_ip_down2.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#         下载ip文件。
+#         下载cf反代ip文件。
 
-if [ -e ./cf_ddns/tmp/ ]; then
-	rm -rf ./cf_ddns/tmp/
-fi
-
-if [ "$IP_SELECT" = "2" ]; then
+if [ "$IP_UPDATE" = "1" ] || [ ! -f ./cf_ddns/ip2/ip.txt ]; then
 	echo "下载cf反代ip文件"
-	if [ ! -d ./cf_ddns/tmp ]; then
-		mkdir ./cf_ddns/tmp
+	if [ -e ./cf_ddns/tmp/ ]; then
+		rm -rf ./cf_ddns/tmp/*
+	else
+		mkdir ./cf_ddns/tmp/
+	fi
+	if [ ! -e ./cf_ddns/ip2/ ]; then
+		mkdir ./cf_ddns/ip2/
 	fi
 	# 定义下载链接和保存路径
 	download_url="https://zip.baipiao.eu.org/"
@@ -51,8 +52,11 @@ if [ "$IP_SELECT" = "2" ]; then
     		cat $extracted_folder/*-1-443.txt > $merged_file
 		# 移动到ip.txt到程序总目录
     		# 将合并后的文件移动到/root/CloudflareST/ip.txt并覆盖原文件
-    		mv -f "$merged_file" "./cf_ddns/ip.txt"
-    		echo "Merged text files containing '-1-443.txt' moved and renamed as 'ip.txt' in ./cf_ddns."
+    		mv -f "$merged_file" "./cf_ddns/ip2/ip.txt"
+    		echo "Merged text files containing '-1-443.txt' moved and renamed as 'ip.txt' in ./cf_ddns/ip2."
 	fi
 	rm -rf ./cf_ddns/tmp/
+	if [ "$IP_SELECT" = "2" ]; then
+		cp -f ./cf_ddns/ip2/ip.txt ./cf_ddns/
+	fi
 fi
