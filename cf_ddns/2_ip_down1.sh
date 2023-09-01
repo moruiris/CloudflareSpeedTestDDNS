2c48c361f23ee56873874593ff1566a54a7e3bb6
moruiris <53965900+moruiris@users.noreply.github.com>
2023-09-01 20:32:31 +0800

    Update 2_ip_down1.sh

--- cf_ddns/2_ip_down1.sh
+++ cf_ddns/2_ip_down1.sh
@@ -19,7 +19,8 @@ if [ "$IP_UPDATE" = "1" ] || [ ! -f ./cf_ddns/ip1/ip.txt ] || [ ! -f ./cf_ddns/i
 	tar -zxf ./cf_ddns/tmp/CloudflareST_linux_*.tar.gz -C ./cf_ddns/tmp/
 	mv -f ./cf_ddns/tmp/ip.txt ./cf_ddns/tmp/ipv6.txt ./cf_ddns/ip1/
 	rm -rf ./cf_ddns/tmp/
-	if [ "$IP_SELECT" = "1" ]; then
-		cp -f ./cf_ddns/ip1/ip.txt ./cf_ddns/ip1/ipv6.txt ./cf_ddns/
-	fi
+fi
+
+if [ "$IP_SELECT" = "1" ]; then
+	cp -f ./cf_ddns/ip1/ip.txt ./cf_ddns/ip1/ipv6.txt ./cf_ddns/
 fi
